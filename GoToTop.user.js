abc48d89f937902bef793c756d99e82ed002a6ab
THR-hub <61302103+THR-hub@users.noreply.github.com>
2023-10-18 12:39:50 +0800

    Update GoToTop.user.js
    fixed a problem that the buttons may occur more than once.

--- GoToTop.user.js
+++ GoToTop.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         网页回到顶部
 // @namespace    https://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  在网页右边增加回到顶部的按钮
 // @author       T_H_R
 // @match        *://*/*
@@ -19,13 +19,13 @@ function createButtonCSS() {
     btn1_css.type = 'text/css';
     btn1_css.innerHTML = `#gm_gotobottom{all:initial;position:fixed;bottom:20%;right:31px;width:12px;background-color:#f7f7f7;border:1px solid grey;
     color:black;border-radius:3px;font-size:12px;padding:3px;cursor:pointer;}`
-    document.head.appendChild(btn1_css);
+    if (self === top){document.head.appendChild(btn1_css);}
 
     var btn2_css = document.createElement('style');
     btn2_css.type = 'text/css';
     btn2_css.innerHTML = `#gm_gototop{all:initial;position:fixed;bottom:20%;right:12px;width:12px;background-color:#f7f7f7;border:1px solid grey;
     color:black;border-radius:3px;font-size:12px;padding:3px;cursor:pointer;}`
-    document.head.appendChild(btn2_css);
+    if (self === top){document.head.appendChild(btn2_css);}
 }
 
 function createButton() {
@@ -33,11 +33,11 @@ function createButton() {
     btn1.id = 'gm_btn-to-bottom';
     btn1.innerHTML = "<div id='gm_gotobottom'>去往底部</div>";
     btn1.onclick = () => window.scrollTo({top:document.documentElement.scrollHeight,behavior:'smooth'});
-    document.body.appendChild(btn1);
+    if (self === top){document.body.appendChild(btn1);}
 
     var btn2 =document.createElement('div');
     btn2.id = 'gm_btn-to-top';
     btn2.innerHTML = "<div id='gm_gototop'>回到顶部</div>";
     btn2.onclick = () => window.scrollTo({top:0,behavior:'smooth'});
-    document.body.appendChild(btn2);
+    if (self === top){document.body.appendChild(btn2);}
 }
