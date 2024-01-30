b51633113aa2482ccea649ee135873febe534c21
THR-hub <61302103+THR-hub@users.noreply.github.com>
2024-01-30 22:10:50 +0800

    Update GoToTop.user.js
    adjust code style; without change on function.

--- GoToTop.user.js
+++ GoToTop.user.js
@@ -5,6 +5,7 @@
 // @description  在网页右边增加回到顶部的按钮
 // @author       T_H_R
 // @match        *://*/*
+// @noframes
 // @grant        none
 // ==/UserScript==
 
@@ -34,12 +35,12 @@ function createButton() {
     btn1.innerHTML = `<div id='gm_gotobottom' style='all:initial;position:fixed;bottom:20%;right:31px;width:12px;background-color:#f7f7f7;
         border:1px solid grey;color:black;border-radius:3px;font-size:12px;padding:3px;cursor:pointer;'>去往底部</div>`;
     btn1.onclick = () => window.scrollTo({top:document.documentElement.scrollHeight,behavior:'smooth'});
-    if (self === top){document.body.appendChild(btn1);}
+    document.body.appendChild(btn1);
 
     var btn2 =document.createElement('div');
     btn2.id = 'gm_btn-to-top';
     btn2.innerHTML = `<div id='gm_gototop' style='all:initial;position:fixed;bottom:20%;right:12px;width:12px;background-color:#f7f7f7;
         border:1px solid grey;color:black;border-radius:3px;font-size:12px;padding:3px;cursor:pointer;'>回到顶部</div>`;
     btn2.onclick = () => window.scrollTo({top:0,behavior:'smooth'});
-    if (self === top){document.body.appendChild(btn2);}
+    document.body.appendChild(btn2);
 }
