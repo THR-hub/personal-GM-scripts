30496237318482762ebb0994d214b425805e743a
THR-hub <61302103+THR-hub@users.noreply.github.com>
2024-03-24 23:02:39 +0800

    Update GoToTop.user.js
    Add hide function. click content in menu to hide.

--- GoToTop.user.js
+++ GoToTop.user.js
@@ -6,8 +6,10 @@
 // @author       T_H_R
 // @match        *://*/*
 // @noframes
-// @grant        none
+// @grant        GM_registerMenuCommand
 // ==/UserScript==
+
+GM_registerMenuCommand('本次隐藏',hideButton);
 
 (function() {
     'use strict';
@@ -15,17 +17,23 @@
 })();
 
 function createButton() {
-    var btn1 =document.createElement('div');
-    btn1.id = 'gm_btn-to-bottom';
-    btn1.innerHTML = `<div id='gm_gotobottom' style='all:initial;position:fixed;bottom:20%;right:31px;width:12px;background-color:#f7f7f7;
-        border:1px solid grey;color:black;border-radius:3px;font-size:12px;padding:3px;cursor:pointer;'>去往底部</div>`;
+    // 暂且保留两种写法，因为我也不知道那种比较好
+    var btn1 = document.createElement('div');
+    btn1.id='btnToEnd';
+    btn1.style=`all:initial;position:fixed;bottom:20%;right:31px;width:12px;background-color:#f7f7f7;
+        border:1px solid grey;color:black;border-radius:3px;font-size:12px;padding:3px;cursor:pointer;`;
+    btn1.innerHTML='去往底部'
     btn1.onclick = () => window.scrollTo({top:document.documentElement.scrollHeight,behavior:'smooth'});
     document.body.appendChild(btn1);
 
     var btn2 =document.createElement('div');
-    btn2.id = 'gm_btn-to-top';
-    btn2.innerHTML = `<div id='gm_gototop' style='all:initial;position:fixed;bottom:20%;right:12px;width:12px;background-color:#f7f7f7;
+    btn2.innerHTML = `<div id='btnToTop' style='all:initial;position:fixed;bottom:20%;right:12px;width:12px;background-color:#f7f7f7;
         border:1px solid grey;color:black;border-radius:3px;font-size:12px;padding:3px;cursor:pointer;'>回到顶部</div>`;
     btn2.onclick = () => window.scrollTo({top:0,behavior:'smooth'});
     document.body.appendChild(btn2);
+}
+
+function hideButton() {
+    document.getElementById('btnToEnd').style.visibility='hidden';
+    document.getElementById('btnToTop').style.visibility='hidden';
 }
