6342d10189b2fe149e035f0cedf0be01f730f043
THR-hub <61302103+THR-hub@users.noreply.github.com>
2024-12-03 03:46:49 +0800

    Update CustomFonts.user.js
    Fixed multi font weight. More tests are needed.

--- CustomFonts.user.js
+++ CustomFonts.user.js
@@ -21,26 +21,29 @@ if(GM_getValue('customFont') === undefined || GM_getValue('customFontUI') === un
 let customFont = GM_getValue('customFont');
 let customFontUI = GM_getValue('customFontUI');
 
-//let style = document.createElement('style');
-//style.type = 'text/css';
+/* 测试用例：
+ * Firefox/Windows11/微软雅黑:100-300,Light;400-500,Regular;600-1000,Bold
+ * Firefox/Windows11/更纱黑体:1-200,XLight;3,Light;4-5,Regular;6,SemiBold;7-10,Bold
+ */
 
-let _css=`@font-face{font-style:normal;font-family:"Arial";src:local("${customFont}"),local("Arial")}
-@font-face{font-style:bolder;font-family:"Arial";src:local("${customFont}"),local("Arial")}
-`;
 // Firefox里sans-serif、system-ui无效，前者可设默认字体，后者无解。Chrome未测试。
+function newCSS(toBeDef, toDef, td2='') { // 不知道为什么Arial总是调用微软雅黑，先写个td2凑合吧
+  return `@font-face{font-family:"${toBeDef}";font-weight:1 1000;src:local("${toDef}")${tbd2}}
+@font-face{font-family:"${toBeDef}";font-weight:1 300;src:local("${toDef} Light")${tbd2}}
+@font-face{font-family:"${toBeDef}";font-weight:400 500;src:local("${toDef}")${tbd2}}
+@font-face{font-family:"${toBeDef}";font-weight:600 1000;src:local("${toDef} Bold")${tbd2}}
+@font-face{font-family:"${toBeDef}";font-weight:100 200;src:local("${toDef} XLight")${tbd2}}
+@font-face{font-family:"${toBeDef}";font-weight:300;src:local("${toDef} Light")${tbd2}}
+@font-face{font-family:"${toBeDef}";font-weight:400 500;src:local("${toDef}")${tbd2}}
+@font-face{font-family:"${toBeDef}";font-weight:600;src:local("${toDef} SemiBold")${tbd2}}
+@font-face{font-family:"${toBeDef}";font-weight:700 1000;src:local("${toDef} Bold")${tbd2}}
+`};
+let _css=newCSS('Arial', customFont, ',local(\"Arial\")')
 for (let s of GM_getValue('replaceFont').concat('sans-serif')) {
-  _css += `@font-face{font-style:normal;font-family:"${s}";src:local("${customFont}")}
-@font-face{font-style:bolder;font-family:"${s}";src:local("${customFont}")}
-`;
+  _css += newCSS(s, customFont);
 }
 for (let s of GM_getValue('replaceFontUI').concat('ui-sans-serif', 'system-ui')) {
-  _css += `@font-face{font-style:normal;font-family:"${s}";src:local("${customFontUI}")}
-@font-face{font-style:bolder;font-family:"${s}";src:local("${customFontUI}")}`
-;
+  _css += newCSS(s, customFontUI);
 }
-
-//style.innerHTML = _css;
-
-//document.head.appendChild(style);
 
 GM_addStyle(_css);
