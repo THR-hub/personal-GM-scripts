fa7006936f619041ee99d815f809d84e8f7eb80c
THR-hub <61302103+THR-hub@users.noreply.github.com>
2025-03-01 02:54:48 +0800

    Update GoToTop.user.js
    combined progress of creating buttons

--- GoToTop.user.js
+++ GoToTop.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         网页回到顶部
 // @namespace    https://tampermonkey.net/
-// @version      0.1.4
+// @version      0.1.5
 // @description  在网页右边增加回到顶部的按钮
 // @author       T_H_R
 // @match        *://*/*
@@ -12,46 +12,31 @@
 // @grant        GM_registerMenuCommand
 // ==/UserScript==
 
-GM_addStyle(`
-@media(prefers-color-scheme: light){.button_GM{background-color: #f7f7f7 !important;color:black !important;}}
-@media(prefers-color-scheme: dark){.button_GM{background-color: #333 !important;color:white !important;}}
-`);
+'use strict';
+
+GM_addStyle(`@media(prefers-color-scheme:light){.button_GM{background-color:#f7f7f7 !important;color:black !important;}}
+@media(prefers-color-scheme:dark){.button_GM{background-color:#333 !important;color:white !important;}}`);
+
+GM_addStyle(`.button_GM{all:initial;display:inline-block;font-size:12px;block-size:auto;border:1px solid grey;border-radius:3px;padding:3px;
+  writing-mode:vertical-rl;letter-spacing:.3em;cursor:pointer;position:fixed;bottom:20%;}#btnToEnd_GM{right:1em;}#btnToTop_GM{right:2.8em;}`)
+
 GM_registerMenuCommand('本次隐藏',hideButton);
 
-(function() {
-    'use strict';
-    createButton();
-})();
-
-function createButton() {
-    var btn1 = document.createElement('div');
-    btn1.id='btnToEnd';
-    btn1.className='button_GM';
-    btn1.lang='zh-Hans-CN';
-    btn1.style=`all:initial;position:fixed;bottom:20%;right:2.6em;width:1em;
-        border:0.1em solid grey;border-radius:0.25em;font-size:12px;padding:0.25em;cursor:pointer;`;
-    btn1.innerHTML='去往底部';
-    btn1.onclick = () => window.scrollTo({top:document.documentElement.scrollHeight,behavior:'smooth'});
-    document.body.appendChild(btn1);
-
-    /* 这种写法不可取，部分网站会重设属性导致显示异常
-    var btn2 =document.createElement('div');
-    btn2.innerHTML = `<div id='btnToTop' class='button_GM' style='all:initial;position:fixed;bottom:20%;right:12px;width:12px;
-        border:1px solid grey;border-radius:3px;font-size:12px;padding:3px;cursor:pointer;'>回到顶部</div>`;
-    btn2.onclick = () => window.scrollTo({top:0,behavior:'smooth'});
-    document.body.appendChild(btn2);*/
-    var btn2 =document.createElement('div');
-    btn2.id='btnToTop';
-    btn2.className='button_GM';
-    btn2.lang='zh-Hans-CN';
-    btn2.style=`all:initial;position:fixed;bottom:20%;right:1em;width:1em;
-        border:0.1em solid grey;border-radius:0.25em;font-size:12px;padding:0.25em;cursor:pointer;`;
-    btn2.innerHTML='回到顶部';
-    btn2.onclick = () => window.scrollTo({top:0,behavior:'smooth'});
-    document.body.appendChild(btn2);
+function createBtn(id,func) {
+  let btn = document.createElement('div');
+  btn.id=id;
+  btn.className='button_GM';
+  btn.lang='zh-Hans-CN';
+  btn.innerHTML='去往底部';
+  btn.onclick=func;
+  document.body.appendChild(btn);
 }
 
 function hideButton() {
-    document.getElementById('btnToEnd').style.visibility='hidden';
-    document.getElementById('btnToTop').style.visibility='hidden';
+    document.getElementById('btnToTop_GM').style.visibility='hidden';
+    document.getElementById('btnToEnd_GM').style.visibility='hidden';
 }
+
+
+createBtn('btnToTop_GM',()=>window.scrollTo({top:0,behavior:'smooth'}));
+createBtn('btnToEnd_GM',()=>window.scrollTo({top:document.documentElement.scrollHeight,behavior:'smooth'}));
