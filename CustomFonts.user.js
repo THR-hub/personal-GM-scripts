fb8abf11d8781cf23911ce970e47d598e0c3973f
THR-hub <thomas_rong@163.com>
2025-07-18 12:00:38 +0800

    add a font to list

--- CustomFonts.user.js
+++ CustomFonts.user.js
@@ -16,7 +16,7 @@ const browser = (/Firefox/.test(navigator.userAgent)) ? 'firefox' : 'chrome';
 const replaceFont = ['Noto Sans CJK SC', 'PingFang SC', 'WenQuanYi Micro Hei',
                      'Microsoft YaHei', '微软雅黑', 'Microsoft JhengHei', '微軟正黑體', 'Meiryo UI', 'Malgun Gothic',
                      'Noto Sans SC', 'Noto Sans JP', 'Noto Sans KR',
-                     'Arial', 'Segoe UI', 'Roboto', 'SF Pro Display',
+                     'Arial', 'Segoe UI', 'Roboto', 'SF Pro Display', 'Tahoma',
                      'SimSun', '宋体', 'SimHei', '黑体', 'STXihei', '华文细黑']; // 理论上Chrome不应替换宋体，暂时不改
 
 if (browser === 'firefox') {
