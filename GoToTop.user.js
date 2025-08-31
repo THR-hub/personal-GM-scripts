1ae112acf6bbc330842240296429ea9312754dcb
THR-hub <thomas_rong@163.com>
2025-08-31 11:26:19 +0800

    rename class name & id to avoid conflict

--- GoToTop.user.js
+++ GoToTop.user.js
@@ -14,34 +14,34 @@
 
 'use strict';
 
-GM_addStyle(`@media(prefers-color-scheme:light){.btn_c_GM{background-color:#f7f7f7 !important;color:black !important;}}
-@media(prefers-color-scheme:dark){.btn_c_GM{background-color:#333 !important;color:white !important;}}`);
+GM_addStyle(`@media(prefers-color-scheme:light){.GM_btn_c{background-color:#f7f7f7 !important;color:black !important;}}
+@media(prefers-color-scheme:dark){.GM_btn_c{background-color:#333 !important;color:white !important;}}`);
 
-GM_addStyle(`#btn_p_GM{all:initial;display:inline-block;font-size:12px !important;
+GM_addStyle(`#GM_btn_p{all:initial;display:inline-block;font-size:12px !important;
   writing-mode:vertical-rl;letter-spacing:.3em;cursor:pointer;position:fixed;bottom:20%;right:max(0px, (1em - 100vw + 100%));z-index:1000}
-  .btn_c_GM{block-size:auto !important;border:.1em solid grey;border-radius:.3em;padding:.3em;position:relative}
-  #btn_2_GM{right:-.1em}`); //边框重叠
+  .GM_btn_c{block-size:auto !important;border:.1em solid grey;border-radius:.3em;padding:.3em;position:relative}
+  #GM_btn_2{right:-.1em}`); //边框重叠
 
 GM_registerMenuCommand('本次隐藏', hideButton);
 
 function hideButton() {
-  document.getElementById('btn_p_GM').style.visibility='hidden';
+  document.getElementById('GM_btn_p').style.visibility='hidden';
 }
 
 
 let btn_p=document.createElement('div');
-btn_p.id='btn_p_GM';
+btn_p.id='GM_btn_p';
 btn_p.lang='zh-Hans-CN';
 
 
 let btn_1=document.createElement('div');
-btn_1.className='btn_c_GM';
+btn_1.className='GM_btn_c';
 btn_1.innerHTML='回到顶部';
 btn_1.onclick=()=>window.scrollTo({top:0,behavior:'smooth'});
 
 let btn_2=document.createElement('div');
-btn_2.id='btn_2_GM';
-btn_2.className='btn_c_GM';
+btn_2.id='GM_btn_2';
+btn_2.className='GM_btn_c';
 btn_2.innerHTML='去往底部';
 btn_2.onclick=()=>window.scrollTo({top:document.documentElement.scrollHeight,behavior:'smooth'});
 
