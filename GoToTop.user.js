499a5d24af37028e5d8557a86d11151e98413a35
Thomas <thomas_rong@163.com>
2026-02-28 16:53:41 +0800

    fix: button text now can't be selected

--- GoToTop.user.js
+++ GoToTop.user.js
@@ -17,7 +17,7 @@
 GM_addStyle(`@media(prefers-color-scheme:light){.GM_btn_c{background-color:#f7f7f7 !important;color:black !important;}}
 @media(prefers-color-scheme:dark){.GM_btn_c{background-color:#333 !important;color:white !important;}}`);
 
-GM_addStyle(`#GM_btn_p{all:initial;display:inline-block;font-size:12px !important;
+GM_addStyle(`#GM_btn_p{all:initial;display:inline-block;font-size:12px !important;user-select:none;
   writing-mode:vertical-rl;letter-spacing:.3em;cursor:pointer;position:fixed;bottom:20%;right:max(0px, (1em - 100vw + 100%));z-index:1000}
   .GM_btn_c{block-size:auto !important;border:.1em solid grey;border-radius:.3em;padding:.3em;position:relative}
   #GM_btn_2{right:-.1em}`); //边框重叠
@@ -37,12 +37,14 @@ btn_p.lang='zh-Hans-CN';
 let btn_1=document.createElement('div');
 btn_1.className='GM_btn_c';
 btn_1.innerHTML='回到顶部';
+btn_1.role='button';
 btn_1.onclick=()=>window.scrollTo({top:0,behavior:'smooth'});
 
 let btn_2=document.createElement('div');
 btn_2.id='GM_btn_2';
 btn_2.className='GM_btn_c';
 btn_2.innerHTML='去往底部';
+btn_2.role='button';
 btn_2.onclick=()=>window.scrollTo({top:document.documentElement.scrollHeight,behavior:'smooth'});
 
 btn_p.appendChild(btn_1);
